56c27aecc04b07f3faecf3597b5861ad440cd10b
jrsahuquillo <jrsahuquillo@gmail.com>
2021-01-30 18:48:58 +0100

    Allows admin to deactivate enabled users

--- app/models/bot_command/manage_user.rb
+++ app/models/bot_command/manage_user.rb
@@ -9,7 +9,8 @@ module BotCommand
     def should_step?
       steps = [
                 'enable_users',
-                'set_users_level'
+                'set_users_level',
+                'disable_users'
               ]
       current_step = user.bot_command_data['step']
       if current_step && steps.include?(current_step)
@@ -28,8 +29,11 @@ module BotCommand
         unleveled_users = User.where(level: nil).sort_by(&:created_at).map{|user| "#{user.username}"}
         rowers = (disabled_users + unleveled_users).uniq
         send_message('Selecciona al socio/a que quieres activar:', set_markup(rowers))
+
       when '/desactivar_socios'
-        byebug
+        user.set_next_step('disable_users')
+        enabled_users = User.where(enabled: true).sort_by(&:created_at).map{|user| "#{user.username}" }
+        send_message('Selecciona al socio/a que quieres desactivar:', set_markup(enabled_users))
       end
     end
 
@@ -45,6 +49,7 @@ module BotCommand
           rower_text = rower.gender == "female" ? "de la remera" : "del remero"
           send_message("Indica el nivel #{rower_text}", set_markup(LEVELS))
         end
+
       when 'set_users_level'
         rower_id = user.get_temporary_data('rower_it_tmp')
         rower = User.find(rower_id)
@@ -53,7 +58,7 @@ module BotCommand
             rower.save
             rower.update_column(:level, text)
             send_message("@#{rower.username} ha sido activado y actualizado con el nivel #{rower.level}")
-            send_update_message(rower)
+            send_state_message(rower, 'enable')
           else
             send_message("Formato de nivel no válido")
             user.reset_step
@@ -62,13 +67,30 @@ module BotCommand
           rower.reset_next_bot_command
           user.reset_next_bot_command
         end
+
+      when 'disable_users'
+        rower = User.find_by(username: text)
+        user.reset_step
+        if rower.present?
+          rower.update_column(:enabled, false)
+          send_message("@#{rower.username} ha sido desactivado")
+          send_state_message(rower, 'disable')
+          rower.reset_next_bot_command
+          user.reset_next_bot_command
+        end
+
       end
     end
 
-    def send_update_message(rower)
-      message = "@#{user.username} ha activado tu cuenta con el nivel *#{rower.level}*.\nUsa /start para ver las opciones"
-      @api.call('sendMessage', chat_id: rower.telegram_id, text: message, reply_markup: nil, parse_mode: 'Markdown')
-
+    def send_state_message(rower, state)
+      if state == 'enable'
+        message = "@#{user.username} ha activado tu cuenta con el nivel *#{rower.level}*.\nUsa /start para ver las opciones"
+        @api.call('sendMessage', chat_id: rower.telegram_id, text: message, reply_markup: nil, parse_mode: 'Markdown')
+      end
+      if state == 'disable'
+        message = "@#{user.username} ha desactivado tu cuenta. Si ha sido un error, escríbele por privado"
+        @api.call('sendMessage', chat_id: rower.telegram_id, text: message, reply_markup: nil, parse_mode: 'Markdown')
+      end
     end
 
     def set_markup(actions)
