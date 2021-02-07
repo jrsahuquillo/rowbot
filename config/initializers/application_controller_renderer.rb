36bad211c7c9277bf68665426f2684ef7389ca34
jrsahuquillo <jrsahuquillo@gmail.com>
2021-02-07 19:30:50 +0100

    Fixes deploying error

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,9 +7,9 @@
 #   )
 # end
 
-credentials = Rails.application.credentials.send(Rails.env)
-host = Rails.application.config.action_controller.default_url_options[:host]
-uri = URI("https://api.telegram.org/bot#{credentials[:bot_token]}/setWebhook?url=#{host}/telegram_#{credentials[:telegram_token]}")
-response = Net::HTTP.get(uri)
-json_response = JSON.parse(response)
-Rails.logger.info("Webhook status: #{json_response['description']}")
+# credentials = Rails.application.credentials.send(Rails.env)
+# host = Rails.application.config.action_controller.default_url_options[:host]
+# uri = URI("https://api.telegram.org/bot#{credentials[:bot_token]}/setWebhook?url=#{host}/telegram_#{credentials[:telegram_token]}")
+# response = Net::HTTP.get(uri)
+# json_response = JSON.parse(response)
+# Rails.logger.info("Webhook status: #{json_response['description']}")
