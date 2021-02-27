53a54519990b6c17627601f8546b74fa7210e191
jrsahuquillo <jrsahuquillo@gmail.com>
2021-02-27 16:07:01 +0100

    Changes help instructions text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'webhooks#main'
   post "/telegram_#{Rails.application.secrets.dig(:telegram_token)}" => 'webhooks#callback'
+  get "/status", to: 'webhooks#main'
 end
