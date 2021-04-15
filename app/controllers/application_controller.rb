6812d478a3193fb07cf046653fdf283c15fe1331
jrsahuquillo <jrsahuquillo@gmail.com>
2021-04-15 08:59:35 +0200

    Redirects to 404 for routes that do not exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def redirect_user
+    redirect_to '/404'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post "/telegram_#{Rails.application.secrets.dig(:telegram_token)}" => 'webhooks#callback'
   get "/status", to: 'webhooks#main'
+
+  get '*path', to: 'application#redirect_user', via: :all
 end
